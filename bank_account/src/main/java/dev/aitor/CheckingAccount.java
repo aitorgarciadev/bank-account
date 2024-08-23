7fec621deae759903057c2a3e80df9ca90ae501a
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-08-23 09:18:28 +0200

    checking account done

---
+++ bank_account/src/main/java/dev/aitor/CheckingAccount.java
@@ -0,0 +1,45 @@
+package dev.aitor;
+
+public class CheckingAccount extends Account {
+    private float overdraft;
+
+    public CheckingAccount(float balance, float annualInterestRate) {
+        super(balance, annualInterestRate);
+        this.overdraft = 0.0f;
+    }
+
+    @Override
+    public void withdraw(float amount) {
+        if (amount > balance) {
+            overdraft += (amount - balance);
+            balance = 0;
+        } else {
+            balance -= amount;
+        }
+        numberOfWithdrawals++;
+    }
+
+    @Override
+    public void deposit(float amount) {
+        if (amount > 0) {
+            if (overdraft > 0) {
+                if (amount >= overdraft) {
+                    amount -= overdraft;
+                    overdraft = 0;
+                } else {
+                    overdraft -= amount;
+                    amount = 0;
+                }
+            }
+            super.deposit(amount);
+        }
+    }
+
+    @Override
+    public String printStatement() {
+        return "Balance: $" + balance + "\n" +
+               "Monthly Fee: $" + monthlyFee + "\n" +
+               "Transactions: " + (numberOfDeposits + numberOfWithdrawals) + "\n" +
+               "Overdraft: $" + overdraft;
+    }
+}
